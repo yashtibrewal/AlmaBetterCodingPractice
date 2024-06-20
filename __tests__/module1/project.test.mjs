cd9f25c59c31aa7d315bbbd4f5f4284d85515247
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-06-20 20:13:05 +0530

    Project 1 completion, including protal provided test cases

--- __tests__/module1/project.test.mjs
+++ __tests__/module1/project.test.mjs
@@ -1,5 +1,300 @@
 import * as project from '../../module1/project.mjs';
 
-test('primes till 5', ()=>{
-    expect(project.findPrimes(5)).toEqual([1,2,3,5]);
-})
+
+
+
+// All test cases assume constrained data and within processing power limit.
+
+/**
+ * Coding Problems Test Cases
+ */
+
+test('BMI to be normal', ()=>{
+    expect(project.calculateBMI(65,1.75)).toEqual("Normal weight");
+});
+
+test('Temperature to be 77.00 F', ()=>{
+    expect(project.convertTemperature(25,'C')).toEqual("77.00 F");
+});
+
+test('Temperature to be -40.00 C', ()=>{
+    expect(project.convertTemperature(-40,'F')).toEqual("-40.00 C");
+});
+
+test('Bill amount to be 75.94', ()=>{
+    expect(project.calculateTip(60.75,0.25)).toEqual("75.94");
+});
+
+test('Bill amount to be 99', ()=>{
+    expect(project.calculateTip(90.00,0.10)).toEqual("99.00");
+});
+
+test('RaceCar is a palindrome', ()=>{
+    expect(project.isPalindrome("RaceCar")).toEqual(true);
+});
+
+test('Hello is not a palindrom', ()=>{
+    expect(project.isPalindrome("Hello")).toEqual(false);
+});
+
+test('Vowels in word', ()=>{
+    expect(project.countVowels('AlmaBetter')).toEqual(4);
+});
+
+test('Vowels in sentence', ()=>{
+    expect(project.countVowels('Coding is fun with fellow learners')).toEqual(10);
+});
+
+/**
+ * Improvised Perspective to the Coding Problem Test Cases
+ */
+
+test('Longest Word Length to be 8', () => {
+    expect(project.findLongestWord("Coding is fun with fellow learners"))
+        .toEqual(8);
+});
+
+test('Longest Word Length to be 6', () => {
+    expect(project.findLongestWord("The quick brown fox jumped over the lazy dog"))
+        .toEqual(6);
+});
+
+test('Title Case 1', () => {
+    expect(project.titleCase("Dream big, work hard, and stay focused"))
+        .toEqual("Dream Big, Work Hard, And Stay Focused");
+});
+
+test('Title Case 2', () => {
+    expect(project.titleCase("Be yourself; everyone else is already taken."))
+        .toEqual("Be Yourself; Everyone Else Is Already Taken.");
+});
+
+
+test('Count Occurrences of a Character to be 3', () => {
+    expect(project.countOccurrences("hello world", 'l'))
+        .toEqual(3);
+});
+
+test('Count Occurrences of a Character to be 3', () => {
+    expect(project.countOccurrences("Elephant", 'E'))
+        .toEqual(1);
+});
+
+test('Shopping Cart Total to be 2200', () => {
+    const cart1 = [
+        { name: "Laptop", price: 1000, quantity: 1 },
+        { name: "Phone", price: 500, quantity: 2 },
+        { name: "Headphones", price: 50, quantity: 4 },
+    ];
+    expect(project.calculateTotal(cart1))
+        .toEqual(2200);
+});
+
+test('Shopping Cart Total to be 114', () => {
+    const cart2 = [
+        { name: "Fruits", price: 10, quantity: 5 },
+        { name: "Vegetables", price: 8, quantity: 3 },
+        { name: "Dairy", price: 5, quantity: 2 },
+        { name: "Snacks", price: 3, quantity: 10 },
+    ];
+    expect(project.calculateTotal(cart2))
+        .toEqual(114);
+});
+
+test('Fizz Buzz for 12', () => {
+
+    expect(project.fizzBuzz(12))
+        .toEqual(["1", "2", "Fizz", "Buzz", "5", "Fizz", "7", "Buzz", "Fizz", "10", "11", "FizzBuzz"]);
+});
+
+test('Fizz Buzz for 5', () => {
+
+    expect(project.fizzBuzz(5))
+        .toEqual( ["1", "2", "Fizz", "Buzz", "5"]);
+});
+
+
+/**
+ * Test cases for project.
+ */
+
+test('primes till 5', () => {
+    expect(project.findPrimes(5)).toEqual([2, 3, 5]);
+});
+
+test('0 is not prime', () => {
+    expect(project.findPrimes(0)).toEqual([]);
+});
+
+test('1 is not prime', () => {
+    expect(project.findPrimes(1)).toEqual([]);
+});
+
+test('primes till 20', () => {
+    expect(project.findPrimes(20)).toEqual([2, 3, 5, 7, 11, 13, 17, 19]);
+});
+
+
+/**
+ * Test cases for reverse string
+ */
+test('reversing Hello', () => {
+    expect(project.reverseString("Hello")).toEqual("olleH");
+});
+test('reversing JavaScript', () => {
+    expect(project.reverseString("JavaScript")).toEqual("tpircSavaJ");
+});
+
+/**
+ * Test cases for signOfProduct.
+ */
+
+test('product of [2, -3, 5, 4] is -1', () => {
+    expect(project.signOfProduct([2, -3, 5, 4])).toEqual(-1);
+});
+
+test('product of [1, 2, 0] is 0', () => {
+    expect(project.signOfProduct([1, 2, 0])).toEqual(0);
+});
+
+test('product of [2, -3, 5, 4, -7] is 1', () => {
+    expect(project.signOfProduct([2, -3, 5, 4, -7])).toEqual(1);
+});
+
+test('product of [2] is 1', () => {
+    expect(project.signOfProduct([2])).toEqual(1);
+});
+
+
+/**
+ * Test cases for check sign.
+ */
+
+test('sign of 2,5,7 is +++', () => {
+    expect(project.checkSign(2, 5, 7)).toEqual('+++');
+});
+
+
+test('sign of -2,5,7 is ++-', () => {
+    expect(project.checkSign(-2, 5, 7)).toEqual('++-');
+});
+
+
+test('sign of -2,5,-7 is +--', () => {
+    expect(project.checkSign(-2, 5, -7)).toEqual('+--');
+});
+
+
+test('sign of -2,-5,-7 is ---', () => {
+    expect(project.checkSign(-2, -5, -7)).toEqual('---');
+});
+
+/**
+ * Test Cases for slug generator
+ */
+
+test('slug of Google', () => {
+    expect(project.slugGenerator("Google")).toEqual("google.com");
+});
+
+test('slug of AlmaBetter Web Dev', () => {
+    expect(project.slugGenerator("AlmaBetter Web Dev")).toEqual("almabetter-web-dev.com");
+});
+
+test('slug of Hello World', () => {
+    expect(project.slugGenerator("Hello World")).toEqual("hello-world.com");
+});
+
+/**
+ * Test Cases for word distance
+ */
+
+test('looking for minimum distance to be 1', () => {
+    expect(project.wordDist(["practice", "makes", "perfect", "coding", "makes"]
+        , "coding", "makes"
+    )).toEqual(1);
+});
+test('looking for minimum distance to be 3', () => {
+    expect(project.wordDist(["apple", "banana", "cherry", "apple", "date", "banana", "fig"]
+        , "apple", "fig"
+    )).toEqual(3);
+});
+
+/**
+ * Test Cases for flip game
+ */
+
+test('looking for 3 ouputs to ++++', () => {
+    const result = project.flipGame("++++");
+    expect(result).toContain("--++");
+    expect(result).toContain("+--+");
+    expect(result).toContain("++--");
+    expect(result.length).toBe(3);
+});
+test('looking for 2 ouputs to ++-++', () => {
+    const result = project.flipGame("++-++");
+    expect(result).toContain("---++");
+    expect(result).toContain("++---");
+    expect(result.length).toBe(2);
+});
+
+/**
+ * Test cases for consequitive characters
+ */
+
+test('consecutive characters for abcdef', () => {
+
+    expect(project.swapConsecutiveCharacters("abcdef")).toEqual("badcfe");
+
+});
+
+test('consecutive characters for AlmaBetter', () => {
+
+    expect(project.swapConsecutiveCharacters("AlmaBetter")).toEqual("lAameBttre");
+
+});
+
+test('consecutive characters for Alumuni', () => {
+
+    expect(project.swapConsecutiveCharacters("Alumuni")).toEqual("lAmunui");
+
+});
+
+/**
+ * Test cases for array intersection
+ */
+
+
+test('array intersection with duplicates', () => {
+    const result = project.arrayIntersection(
+        [1, 2, 2, 3, 4, 5], [2, 3, 3, 6]
+    );
+    expect(result).toContain(2);
+    expect(result).toContain(3);
+    expect(result.length).toBe(2);
+
+});
+
+test('array intersection without duplicates', () => {
+    const result = project.arrayIntersection(
+        [70, 20, 30, 50], [30, 40, 50, 60, 70]
+    );
+    expect(result).toContain(30);
+    expect(result).toContain(50);
+    expect(result).toContain(70);
+    expect(result.length).toBe(3);
+
+});
+
+
+/**
+ * Test cases for insert 7
+ */
+
+test('inserts 7 to Hello World and Universe!', () => {
+    expect(project.insertSeven("Hello World and Universe!")).toEqual("Hello W7orld an7d Unive7rse!");
+});
+
+test('inserts 7 to Full Stack Web Development', () => {
+    expect(project.insertSeven("Full Stack Web Development")).toEqual("Full St7ack Web7 Develo7pment");
+});
--- module1/project.mjs
+++ module1/project.mjs
@@ -1,24 +1,162 @@
+
+export function calculateBMI(weight, height) {
+    // Write your code here
+    const bmi = weight / (height * height);
+
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        return "Normal weight";
+    } else if (bmi >= 25 && bmi < 29.9) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+}
+
+
+export function convertTemperature(temperature, unit) {
+    // Write your code here
+    if (unit === "C") {
+        const fahrenheit = temperature * 9 / 5 + 32;
+        return fahrenheit.toFixed(2) + " F";
+    } else if (unit === "F") {
+        const celsius = (temperature - 32) * 5 / 9;
+        return celsius.toFixed(2) + " C";
+    } else {
+        return "Invalid unit. Use 'C' for Celsius or 'F' for Fahrenheit.";
+    }
+}
+
+
+export function calculateTip(billAmount, tipPercentage) {
+    // Write your code here
+    let tipAmount = billAmount * tipPercentage;
+    let totalAmount = billAmount + tipAmount;
+    return totalAmount.toFixed(2);
+}
+
+
+export function isPalindrome(str) {
+    // Write your code here
+    const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const reversedStr = cleanedStr.split("").reverse().join("");
+    return cleanedStr == reversedStr;
+}
+
+export function countVowels(str) {
+    // Write your code here
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const lowerStr = str.toLowerCase();
+    let count = 0;
+
+    for (let i = 0; i < lowerStr.length; i++) {
+        if (vowels.includes(lowerStr[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+/**
+ * Improvised Perspective to the Coding Problem
+ */
+
+export function findLongestWord(sentence) {
+    // Write your code here
+    const words = sentence.split(' ');
+    let maxLength = 0;
+
+    for (let word of words) {
+        const length = word.length;
+        if (length > maxLength) {
+            maxLength = length;
+        }
+    }
+
+    return maxLength;
+}
+
+// Do not modify the below lines
+// module.exports = { findLongestWord };
+
+export function titleCase(sentence) {
+    // write your code here
+    const words = sentence.toLowerCase().split(' ');
+
+    return words.map((word) => word[0].toUpperCase() + word.substring(1)).join(' ');
+
+}
+
+// Do not modify the below lines
+// module.exports = { titleCase };
+
+export function countOccurrences(str, char) {
+
+    return str.split("").filter((item) => item == char).length;
+}
+
+// Do not modify the below lines
+// module.exports = { countOccurrences };
+
+export function calculateTotal(cart) {
+
+    return cart.reduce((total, cart) => total + (cart.price * cart.quantity), 0)
+}
+
+// Do not modify the below lines
+// module.exports = { calculateTotal };
+
+export function fizzBuzz(n) {
+    const result = [];
+
+    for (let i = 1; i <= n; i++) {
+        switch (i % 3 === 0 && i % 4 === 0) {
+            case true: result.push("FizzBuzz"); break;
+            case false:
+                switch (i % 3 === 0) {
+                    case true: result.push("Fizz");
+                        break;
+                    case false:
+                        switch (i % 4 == 0) {
+                            case true:
+                                result.push("Buzz");
+                                break;
+                            case false:
+                                result.push(i.toString());
+                                break;
+                        }
+                        break;
+                }
+                break;
+        }
+    }
+
+    return result;
+}
+
+// Do not modify the below lines
+// module.exports = { fizzBuzz };
 /**
  * Logic Build Coding Questions
  */
 
 // Write a JavaScript function findPrimes that takes a positive integer n as input and returns an array of prime numbers less than or equal to n.
-export function findPrimes(num){
+export function findPrimes(num) {
 
-    function isPrime(num){
+    function isPrime(num) {
 
-        if(num == 1 || num == 2){
+        if (num == 1 || num == 2) {
             return true;
         }
-    
-        if(typeof num =="number"){
-    
-            for(let i=2;i<num;i++){
-                if(num%i==0){
+
+        if (typeof num == "number") {
+
+            for (let i = 2; i < num; i++) {
+                if (num % i == 0) {
                     return false;
                 }
             }
-    
+
             return true;
         }
         return false;
@@ -26,12 +164,182 @@ export function findPrimes(num){
 
     const primes = [];
 
-    for(let i=1;i<=num;i++){
+    for (let i = 2; i <= num; i++) {
 
-        if(isPrime(i)){
+        if (isPrime(i)) {
             primes.push(i);
         }
 
     }
     return primes;
+}
+
+// Write a JavaScript function reverseString that takes a string as input and returns the string with its characters reversed.
+export function reverseString(str) {
+    let result = '';
+    if (typeof str == "string") {
+        result = str.split("").reverse().join("");
+    }
+    return result;
+}
+
+// Write a JavaScript function signOfProduct that takes an array of integers as input and returns the sign of the product of all the integers in the array.
+export function signOfProduct(arr) {
+
+    let sign = 1;
+
+    if (Array.isArray(arr)) {
+        if (arr.includes(0)) {
+            sign = 0;
+
+        }
+        else {
+
+            for (let item of arr) {
+                if (item > 0) {
+                    continue;
+                } else {
+                    sign = sign == -1 ? 1 : -1;
+                }
+            }
+        }
+
+    }
+    return sign;
+
+}
+
+// Write a JavaScript function checkSign that takes three numbers as input and returns a string representing the signs of the numbers.
+export function checkSign(...args) {
+
+    let result = ["+", "+", "+"];
+
+    for (let item of args) {
+        if (item < 0) {
+            result.shift();
+            result.push("-");
+        }
+    }
+    return result.join("");
+}
+
+// Imagine you're working on a blog platform. Write a JavaScript function generateSlug that takes a title string and generates a URL-friendly slug by replacing spaces with hyphens and converting the text to lowercase.
+export function slugGenerator(word) {
+    let result = '';
+
+    if (typeof word == 'string') {
+        result = word.toLowerCase().split(" ").join("-").concat(".com");
+    }
+    return result;
+}
+
+// Given an array of strings wordsDict and two distinct strings word1 and word2, which are both present in the array. Write a JavaScript function shortestDistance that returns the minimum distance between the positions of word1 and word2 within the list of words.
+export function wordDist(arr, wordOne, wordTwo) {
+    let result = -1;
+    if (Array.isArray(arr)) {
+        const acc1 = [];
+        const acc2 = [];
+        arr.reduce(
+            (acc, item, index) => {
+                if (item == wordOne) {
+                    acc.push(index);
+                }
+                return acc;
+            }, acc1
+        )
+        arr.reduce(
+            (acc, item, index) => {
+                if (item == wordTwo) {
+                    acc.push(index);
+                }
+                return acc;
+            }, acc2
+        )
+        if (acc1.length && acc2.length) {
+            result = Math.min();
+            for (let dist1 of acc1) {
+                for (let dist2 of acc2) {
+                    if (Math.abs(dist1 - dist2) < result) {
+                        result = Math.abs(dist1 - dist2);
+                    }
+                }
+            }
+        }
+    }
+    return result;
+}
+
+// You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
+// Write a JavaScript function findMove that takes a string as input and calculates all the possible states of the string after a valid move has been made and return the states in an array of strings.
+export function flipGame(str) {
+    const result = [];
+    function flip(arr, pos1, pos2) {
+        const copy = [...arr];
+        copy[pos1] = arr[pos1] == '+' ? '-' : '+';
+        copy[pos2] = arr[pos2] == '+' ? '-' : '+';
+        return copy.join("");
+    }
+    const strArray = str.split("");
+    let res = [];
+    for (let i = 0; i < strArray.length - 1; i++) {
+        if (strArray[i] == strArray[i + 1]) {
+            res = flip(strArray, i, i + 1);
+            result.push(res);
+        }
+    }
+    return result;
+}
+
+// Write a JavaScript function swapConsecutiveCharacters that takes a string as input and rearranges the consecutive characters within the string in a swapped manner and returns a swapped string.
+export function swapConsecutiveCharacters(str) {
+
+    let result = '';
+    if (typeof str == 'string')
+        for (let i = 0; i < str.length; i += 2) {
+
+            if (i + 1 < str.length) {
+                result += str[i + 1];
+                result += str[i];
+            } else {
+                result += str[i];
+            }
+
+        }
+
+
+    return result;
+}
+
+export function arrayIntersection(arrOne, arrTwo) {
+
+    const setOne = new Set(arrOne);
+    const result = [];
+    for (let item of setOne) {
+        if (arrTwo.includes(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+}
+
+export function insertSeven(str) {
+
+    let result = "";
+    let counter = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        result += str[i];
+
+        if (/^[a-zA-Z]$/.test(str[i])) {
+            counter++;
+            if (counter == 6) {
+                result += "7";
+                counter = 0;
+            }
+        }
+
+    }
+
+    return result;
+
 }
