2aba914788ff883291983111abd26809facf9b68
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-06-25 23:46:06 +0530

    update

--- codingplatformquestions/coding_assessment/callStackStages.mjs
+++ codingplatformquestions/coding_assessment/callStackStages.mjs
@@ -1,15 +1,22 @@
 // STAGE 1  : Call stack is empty. // The code does not terminate.
 
 function greeting() {
-    sayHi();
+    return sayHi();
 }
 function sayHi() {
     return "Hi!";
 }
+setTimeout(() => {
+    console.log(sayHi());
+}, 1000);
 
-// STAGE 2  : Call stack is empty. // The code does not terminate.
+// STAGE 2  : Call stack is empty. // The code does not terminate. // Heap has 1 function though
 
-greeting();
+console.log(greeting());
 // STAGE 3 : Call stack fills up with greetings and sayhi, then pops them goes to last stage.
 
-// STAGE 4  : Call stack is empty. // The code terminates here.
+// STAGE 4 : Call stack is empty. // Program does not terminate // Heap has 1 function though
+ 
+// STAGE 5 : Call stack takes in the heap event, i.e. the set timeout event.
+
+// STAGE 6  : Call stack is empty AND heap is empty. // The code terminates here.
